f4c6a1f5afffd110bd274bc8e32a2e2ccd5fa4b2
zzl <1819011603@qq.com>
2021-05-20 21:22:15 +0800

    Bill类建立了java类与mysql中的bill表的映射

--- src/mhl/javaBean/Bill.java
+++ src/mhl/javaBean/Bill.java
@@ -7,6 +7,7 @@ import java.util.Date;
  * @author 18190
  * @Date: 2021/5/20  11:53
  * @VERSION 1.0
+ * 账单的javaBean
  */
 public class Bill {
     private Integer id;
@@ -18,6 +19,30 @@ public class Bill {
     private Date billDate;
     private String state;
 
+    /**
+     *
+     * @param id 主键id
+     * @param billId 账单id名 利用UUID生成
+     * @param menuId 与menu表的id相等 menu表示菜名的表
+     * @param nums  菜的数量
+     * @param money 菜的单价
+     * @param diningTableId 该订单是哪个桌号生成的
+     * @param billDate 账单的生成日期
+     * @param state 订单的支付状态 有 未结账、支付宝、微信、现金、流帐等状态
+     */
+    public Bill(Integer id, String billId, Integer menuId, Integer nums, Double money, Integer diningTableId, Date billDate, String state) {
+        this.id = id;
+        this.billId = billId;
+        this.menuId = menuId;
+        this.nums = nums;
+        this.money = money;
+        this.diningTableId = diningTableId;
+        this.billDate = billDate;
+        this.state = state;
+    }
+
+    public Bill() {
+    }
     @Override
     public String toString() {
         return
@@ -94,17 +119,6 @@ public class Bill {
         this.state = state;
     }
 
-    public Bill(Integer id, String billId, Integer menuId, Integer nums, Double money, Integer diningTableId, Date billDate, String state) {
-        this.id = id;
-        this.billId = billId;
-        this.menuId = menuId;
-        this.nums = nums;
-        this.money = money;
-        this.diningTableId = diningTableId;
-        this.billDate = billDate;
-        this.state = state;
-    }
 
-    public Bill() {
-    }
+
 }
